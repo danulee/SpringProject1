8117ea08d66a4c650a9858a8dc1f4687b26771ad
danulee <idanu9879@gmail.com>
2022-07-26 21:07:14 +0900

    수정페이지에서 추천수 노출

--- src/main/java/com/khj/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/khj/exam/demo/repository/ArticleRepository.java
@@ -14,13 +14,22 @@ public interface ArticleRepository {
 	public void writeArticle(@Param("memberId") int memberId, @Param("boardId") int boardId, @Param("title") String title, @Param("body") String body);
 	
 	@Select("""
+			<script>
 			SELECT A.*,
-			M.nickname AS extra__writerName
+			M.nickname AS extra__writerName,
+			IFNULL(SUM(RP.point), 0) AS extra__sumReactionPoint,
+			IFNULL(SUM(IF(RP.point &gt; 0, RP.point, 0)), 0) AS extra__goodReactionPoint,
+			IFNULL(SUM(IF(RP.point &lt; 0, RP.point, 0)), 0) AS extra__badReactionPoint
 			FROM article AS A
-			LEFT JOIN member AS M
+			LEFT JOIN `member` AS M
 			ON A.memberId = M.id
+			LEFT JOIN reactionPoint AS RP
+			ON RP.relTypeCode = 'article'
+			AND A.id = RP.relId
 			WHERE 1
 			AND A.id = #{id}
+			GROUP BY A.id
+			</script>
 			""")
 	public Article getForPrintArticle(@Param("id") int id);
 
@@ -61,18 +70,18 @@ public interface ArticleRepository {
 						</otherwise>
 					</choose>
 				</if>
-				<if test="limitTake != -1">
-					LIMIT #{limitStart}, #{limitTake}
-				</if>
 			) AS A
 			LEFT JOIN reactionPoint AS RP
 			ON RP.relTypeCode = 'article'
 			AND A.id = RP.relId
 			GROUP BY A.id
 			ORDER BY A.id DESC
+			<if test="limitTake != -1">
+			LIMIT #{limitStart}, #{limitTake}
+			</if>
 			</script>
 			""")
-	public List<Article> getArticles(int boardId, String searchKeyword, String searchKeywordTypeCode, int limitStart, int limitTake);
+	public List<Article> getForPrintArticles(int boardId, String searchKeyword, String searchKeywordTypeCode, int limitStart, int limitTake);
 
 	public int getLastInsertId();
 
