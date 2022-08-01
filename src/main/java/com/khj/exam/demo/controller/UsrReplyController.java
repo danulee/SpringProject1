45623d8a0ea4cb2060a1dcd8ab8b848e494f692f
danulee <idanu9879@gmail.com>
2022-08-01 21:50:58 +0900

    댓글 수정 구현

--- src/main/java/com/khj/exam/demo/controller/UsrReplyController.java
+++ src/main/java/com/khj/exam/demo/controller/UsrReplyController.java
@@ -1,23 +1,27 @@
 package com.khj.exam.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.khj.exam.demo.service.ArticleService;
 import com.khj.exam.demo.service.ReplyService;
 import com.khj.exam.demo.utill.Ut;
-import com.khj.exam.demo.vo.Member;
+import com.khj.exam.demo.vo.Article;
+import com.khj.exam.demo.vo.Reply;
 import com.khj.exam.demo.vo.ResultData;
 import com.khj.exam.demo.vo.Rq;
-import com.khj.exam.demo.vo.Reply;
 
 @Controller
 public class UsrReplyController {
 	private ReplyService replyService;
+	private ArticleService articleService;
 	private Rq rq;
 
-	public UsrReplyController(ReplyService replyService, Rq rq) {
+	public UsrReplyController(ReplyService replyService, ArticleService articleService, Rq rq) {
 		this.replyService = replyService;
+		this.articleService = articleService;
 		this.rq = rq;
 	}
 
@@ -78,5 +82,70 @@ public class UsrReplyController {
 		}
 
 		return rq.jsReplace(deleteReplyRd.getMsg(), replaceUri);
+	}
+	
+	@RequestMapping("/usr/reply/modify")
+	public String modify(int id, String replaceUri, Model model) {
+		if ( Ut.empty(id) ) {
+			return rq.jsHistoryBack("id(을)를 입력해주세요.");
+		}
+
+		Reply reply = replyService.getForPrintReply(rq.getLoginedMemberId(), id);
+
+		if ( reply == null ) {
+			return rq.jsHistoryBack(Ut.f("%d번 댓글은 존재하지 않습니다.", id));
+		}
+
+		if ( reply.isExtra__actorCanDelete() == false ) {
+			return rq.jsHistoryBack(Ut.f("%d번 댓글을 수정할 권한이 없습니다.", id));
+		}
+
+		String relDataTitle = null;
+
+		switch (reply.getRelTypeCode()) {
+		case "article":
+			Article article = articleService.getArticle(reply.getRelId());
+			relDataTitle = article.getTitle();
+		}
+
+		model.addAttribute("relDataTitle", relDataTitle);
+		model.addAttribute("reply", reply);
+
+
+		return "usr/reply/modify";
+	}
+	
+	@RequestMapping("/usr/reply/doModify")
+	@ResponseBody
+	public String doModify(int id, String body, String replaceUri) {
+		if ( Ut.empty(id) ) {
+			return rq.jsHistoryBack("id(을)를 입력해주세요.");
+		}
+
+		Reply reply = replyService.getForPrintReply(rq.getLoginedMemberId(), id);
+
+		if ( reply == null ) {
+			return rq.jsHistoryBack(Ut.f("%d번 댓글은 존재하지 않습니다.", id));
+		}
+
+		if ( reply.isExtra__actorCanDelete() == false ) {
+			return rq.jsHistoryBack(Ut.f("%d번 댓글을 수정할 권한이 없습니다.", id));
+		}
+
+		if ( Ut.empty(body) ) {
+			return rq.jsHistoryBack("body(을)를 입력해주세요.");
+		}
+
+		ResultData modifyReplyRd = replyService.modifyReply(id, body);
+
+		if ( Ut.empty( replaceUri )) {
+			switch (reply.getRelTypeCode()) {
+			case "article":
+				replaceUri = Ut.f("../article/detail?id=%d", reply.getRelId());
+				break;
+			}
+		}
+
+		return rq.jsReplace(modifyReplyRd.getMsg(), replaceUri);
 	}
 }
